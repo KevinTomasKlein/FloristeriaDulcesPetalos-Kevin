1f060e71970fc5e7707783f9a376259033f27c98
KevinTomasKlein <kevin.tomas@biko2.com>
2021-04-16 10:14:28 +0200

    Kevin:Cambios

--- floristeria/src/App.tsx
+++ floristeria/src/App.tsx
@@ -4,6 +4,7 @@ import { FormFloristeria } from "./FormFloristeria";
 function App() {
   return (
     <div className="container-fluid">
+      <header className=""></header>
       <FormFloristeria />
     </div>
   );
